1289e0bb8505e58e2c1c1bad65773e13166dcc63
Jeremias01 <147272167+Jeremias01@users.noreply.github.com>
2025-11-11 12:22:08 -0300

    jj

--- labo08_SVD.py
+++ labo08_SVD.py
@@ -28,21 +28,31 @@ def svd_reducida(A, k = "max", tol = 1e-15):
 
     return hatU, hatSig, hatV
 
-
     # No esta testeada
 
+
 def svd_completa(A, tol = 1e-15):
-    V,sigma=diagRH(A,tol)
-    B=matmul(A,V)
+    V,Avals=diagRH(A,tol) # matriz de autovect y de avals
+    Sigma=[]
+    for i in range(len(Avals)):
+        if(Avals[i][i]>=tol):   
+            Sigma.append=sqr(Avals[i][i])   # asigno a sigma sus valores singulares
+    B=matmul(A,V)   
     normalizado=[]
     suma=np.zeros(len(B[0]))
-    for i in traspuesta(B):
+    for i in traspuesta(B): # por cada i-esima columna
         if(norma(i,2)!=0):
-            normalizado.append(i/norma(i,2))
-    ceros=np.zeros(len(B)-len(normalizado))
-    if(len(normalizado)<len(B)-1):
-        ceros[0]=1
-        normalizado.append(normalizado[0]-proyectar(ceros,normalizado[0]))
-        ceros[0]=0
-        for i in range(len(normalizado),len(B)-1):            
-            
+            normalizado=normalizado.append(i/norma(i,2))    #normalizo
+    while(len(normalizado)<len(B)):       # hasta que normalizado esté completo
+        canonica=np.zeros(len(normalizado))
+        for i in range(len(normalizado),len(B)):    
+            if(norma(normalizado[i-len(normalizado)],2)!=0):
+                canonica[i-len(normalizado)]=1      # creo el canonico
+                suma=suma+proyectar(canonica,normalizado[i-len(normalizado)])       # voy sumando las proyecciones del canonico en los vectores del espacio ortonormal hasta ahora
+                normalizado=normalizado.append(normalizado[i-len(normalizado)]-suma)        # agrego un vector ortonormal
+                canonica[i-len(normalizado)]=0      # reinicio a ceros el canonico
+
+    U=normalizado       #cambio de nombre para el return
+    return U,Sigma,V
+
+    # No esta testeada
